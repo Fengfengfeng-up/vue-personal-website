d93c4957db35e4dd9637ea50f4b2597cce5ef75e
冯风风 <1010423552@qq.com>
2021-03-08 17:31:16 +0800

    Change background style

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" :class="{ 'page-cat': $route.name === 'Cat' }">
     <keep-alive>
       <router-view />
     </keep-alive>
@@ -18,3 +18,20 @@ export default {
   }
 }
 </script>
+
+<style>
+#app.page-cat::before {
+  content: '';
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0.05;
+  z-index: -1;
+  background-image: url('./assets/image/bg.png');
+  background-size: 200px 200px;
+  background-repeat: repeat;
+  background-position: center center;
+}
+</style>
