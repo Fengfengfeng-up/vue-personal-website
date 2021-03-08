d16d9d5bf17c3f0441ea0305b61f430c7c4ca670
冯风风 <1010423552@qq.com>
2021-03-08 20:22:29 +0800

    Add footer info

--- src/layout/components/Footer.vue
+++ src/layout/components/Footer.vue
@@ -1,6 +1,9 @@
 <template functional>
   <div class="layout-footer">
-    <!-- <div class="footer-text"><span>滇ICP备20001313号-1</span><span>Fenghan</span>2020 ©</div> -->
+    <div class="text">
+      © <span>Fenghan</span> 2020-2021, powered by
+      <span>Vue</span>
+    </div>
   </div>
 </template>
 
@@ -8,26 +11,20 @@
 .layout-footer {
   grid-area: footer;
   padding: 0 1rem;
-  // display: flex;
-  // justify-content: center;
-  // align-items: flex-end;
-  // font-size: var(--font-smaller);
-  // .footer-text {
-  //   padding: 1rem;
-  //   // text-shadow: 1px 1px 1px var(--gray);
-  //   color: var(--blue-darker);
-  //   span {
-  //     font-weight: 500;
-  //     letter-spacing: 1px;
-  //     margin-right: 0.5rem;
-  //     &:first-child {
-  //       margin-right: 1rem;
-  //     }
-  //   }
-  // }
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  font-size: var(--font-smaller);
+  .text {
+    padding: 1rem;
+    color: var(--gray);
+    span {
+      font-weight: 500;
+      letter-spacing: 1px;
+    }
+  }
 }
-// body.dark-mode .footer-text {
-//   color: var(--gray);
-//   text-shadow: none;
-// }
+body.dark-mode .text {
+  color: var(--gray-dark);
+}
 </style>
